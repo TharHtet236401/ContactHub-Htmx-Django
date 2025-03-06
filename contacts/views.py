b3275576526036a8a6097297145422783fb0e203
TharHtet236401 <tharhteta3@gmail.com>
2025-03-07 01:24:13 +0700

    add comments

--- contacts/views.py
+++ contacts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 # Create you@ views here.
 @login_required
 def index(request):
+    #get all contacts for the current user
     contacts= request.user.contacts.all().order_by('-created_at')
     context = {
         'contacts': contacts
